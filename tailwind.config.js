842c50889c4294d4a115947d0a9e36b4a1af28ec
Muhammad Danial <mhd2danial3@gmail.com>
2025-01-17 17:57:02 +0700

    install daisyUi and scroll animation

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,11 +2,23 @@
 
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./docs/**/*.{html,js}'],
+  safelist: [
+    '!duration-[0ms]',
+    '!delay-[0ms]',
+    'html.js :where([class*="taos:"]:not(.taos-init))'
+  ],
+  content: {
+    relative: true,
+    transform: (content) => content.replace(/taos:/g, ''),
+    files : ['./docs/**/*.{html,js}']
+  },
   theme: {
     extend: {
     },
   },
-  plugins: [],
+  plugins: [
+    require('daisyui'),
+    require('taos/plugin')
+  ],
 }
 
