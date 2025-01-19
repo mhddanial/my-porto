d454548f9963cddd6e27773ba6919e47c45a076a
Muhammad Danial <mhd2danial3@gmail.com>
2025-01-19 22:17:04 +0700

    add navigation bar (-responsive)

--- tailwind.config.js
+++ tailwind.config.js
@@ -13,7 +13,14 @@ module.exports = {
     files : ['./docs/**/*.{html,js}']
   },
   theme: {
+    container: {
+      center: true,
+      padding: '2rem',
+    },
     extend: {
+      screens: {
+        '2xl': '1320px',
+      },
     },
   },
   plugins: [
