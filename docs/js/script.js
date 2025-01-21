f2cf0e910ed51bee3762e1d7c41a95c986400642
Muhammad Danial <mhd2danial3@gmail.com>
2025-01-21 22:59:59 +0700

    navbar done

--- docs/js/script.js
+++ docs/js/script.js
@@ -8,4 +8,9 @@
         progressBar.style.width = progress + "%";
     });
 
-    // 
+    // hamburger menu
+    const hamburger = document.querySelector("#hamburger");
+    const navMenu = document.querySelector("#nav-menu");
+        hamburger.addEventListener("click", function () {
+        navMenu.classList.toggle("hidden");
+    });
